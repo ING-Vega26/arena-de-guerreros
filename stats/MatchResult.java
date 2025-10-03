90a36f084c4503805522c736832c733c03405a43
Brayan Vega <brayan2@gmail.com>
2025-10-03 08:37:40 -0500

    a

---
+++ stats/MatchResult.java
@@ -0,0 +1,45 @@
+package stats;
+
+public class MatchResult {
+    private String playerName;
+    private int totalDamage;
+    private int victories;
+    private long duration; // en segundos
+
+    public MatchResult(String playerName, int totalDamage, long duration) {
+        this.playerName = playerName;
+        this.totalDamage = totalDamage;
+        this.duration = duration;
+        this.victories = 0; // al iniciar no tiene victorias
+    }
+
+    // 📌 Métodos de actualización
+    public void addDamage(int dmg) {
+        this.totalDamage += dmg;
+    }
+
+    public void addVictory() {
+        this.victories++;
+    }
+
+    public void setDuration(long d) {
+        this.duration = d;
+    }
+
+    // 📌 Getters
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getTotalDamage() {
+        return totalDamage;
+    }
+
+    public int getVictories() {
+        return victories;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+}
